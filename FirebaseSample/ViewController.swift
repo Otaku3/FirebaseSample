000d2c9867ba561e08f2dc189e88c8b04c5f31f7
otakumi <otakumi@m.cs.osakafu-u.ac.jp>
2018-12-12 17:30:42 +0900

    Initial Commit

---
+++ FirebaseSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseSample
+//
+//  Created by 大林拓実 on 2018/12/12.
+//  Copyright © 2018年 TakumiObayashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
