9175bac8d1da1c34de68aad92c3c81fc7f201531
otakumi <otakumi@m.cs.osakafu-u.ac.jp>
2018-12-12 18:36:52 +0900

    データの更新(updataChildValues) 名前と年齢で独立して書き替え

--- FirebaseSample/ViewController.swift
+++ FirebaseSample/ViewController.swift
@@ -13,7 +13,9 @@ import FirebaseDatabase
 class ViewController: UIViewController {
     
     //入力フィールド
-    @IBOutlet weak var TextField: UITextField!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var ageTextField: UITextField!
+    
     @IBOutlet weak var displayData: UILabel!
     
     //インスタンス変数
@@ -27,11 +29,19 @@ class ViewController: UIViewController {
         DBRef = Database.database().reference()
     }
     
-    @IBAction func add(_ sender: AnyObject){
-        let data = ["name": TextField.text!]
-        DBRef.child("user/01").setValue(data)
+    @IBAction func addName(_ sender: AnyObject){
+        let data = ["name": nameTextField.text!]
+        DBRef.child("user/01").updateChildValues(data)
         
         let defaultPlace = DBRef.child("user/01/name")
+        defaultPlace.observe(.value){ (snap: DataSnapshot) in self.displayData.text = (snap.value! as AnyObject).description}
+    }
+    
+    @IBAction func addAge(_ sender: AnyObject){
+        let data = ["age": ageTextField.text!]
+        DBRef.child("user/01").updateChildValues(data)
+        
+        let defaultPlace = DBRef.child("user/01/age")
         defaultPlace.observe(.value){ (snap: DataSnapshot) in self.displayData.text = (snap.value! as AnyObject).description}
     }
     
